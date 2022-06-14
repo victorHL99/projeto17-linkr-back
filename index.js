b2f738ac6af94aeacecae80245edcc1ed66a8946
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-14 11:46:54 -0300

    <feat> : publish router created

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
 
+import router from "./routes/index.js";
+
 const app = express();
+dotenv.config();
 
 const port = process.env.PORT || 4000;
 
 app.listen(port,()=>{
+    console.log(chalk.red(`Mode : ${process.env.MODE || "DEV"}`));
     console.log(chalk.green(`Server is running on port ${port}`));
 })
---
+++ routes/publishRouter.js
@@ -0,0 +1,6 @@
+import {Router} from 'express';
+
+const publishRouter = Router();
+console.log(" Passando pela rota de publicação ");
+
+publishRouter.post("/publish")
