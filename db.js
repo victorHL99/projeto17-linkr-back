0882439ff00ae608377f52ad4427e95498cc76c1
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-14 11:13:36 -0300

    <feat> : file database

---
+++ db.js
@@ -0,0 +1,51 @@
+// CONFIGURAÇÕS PARA SUBIR PARA O HEROKU
+/*
+import pg from 'pg';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {Pool} = pg;
+
+const database = {
+    connectionString: process.env.DATABASE_URL,
+}
+
+if(process.env.MODE === "PROD"){
+    databaseConfig.ssl ={
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(databaseConfig)
+
+export default db;
+*/
+
+// CONFIGURAÇÕES PARA TESTAR LOCAL
+
+/*
+import pg from 'pg';
+import chalk from 'chalk';
+
+const {Pool} = pg;
+
+const user = 'postgres';
+const password = 'postgres';
+const host = 'localhost';
+const port = 5432;
+const database = 'linkr';
+
+const db = new Pool({
+    host,
+    port,
+    user,
+    password,
+    database,
+})
+
+console.log(chalk.green(`DATABASE CONNECTED`));
+
+export default db;
+*/
