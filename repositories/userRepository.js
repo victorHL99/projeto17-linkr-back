de8348707bae038c473882dff3e1bf4e1bff240e
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-14 13:16:14 -0300

    <feat> : post publication awaiting tests

---
+++ repositories/userRepository.js
@@ -0,0 +1,16 @@
+import db from "../db.js";
+
+async function getIdUserByToken(token){
+    return db.query(`
+        SELECT users.id 
+        FROM users
+        JOIN sessions
+        ON sessions."user_id" = users.id
+        WHERE sessions.token = $1`, [token]);
+};
+
+const userRepository = {
+    getIdUserByToken,
+}
+
+export default userRepository;
