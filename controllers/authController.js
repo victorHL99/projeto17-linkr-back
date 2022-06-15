1fcb4187bf6d6f84bc08b67c5903d3e600519465
Alyson Consuli <consulialyson@gmail.com>
2022-06-15 16:21:02 -0300

    feat: signin with middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,41 @@
-import bcrypt from "bcrypt";
-//import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
-import authRepository from "../repositories/authRepository.js";
+import db from "../config/db.js"
+import authRepository from "../repositories/authRepository.js"
 
 export const postUser = async (req, res) => {
-  const { username, email, password, profile_image } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const { username, email, password, profile_image } = req.body
+  const hashedPassword = bcrypt.hashSync(password, 10)
   try {
     await authRepository.insertUserDb(
       username,
       email,
       hashedPassword,
-      profile_image
-    );
-    res.sendStatus(201);
+      profile_image,
+    )
+    res.sendStatus(201)
   } catch (e) {
-    return res.status(500).send(e.message);
+    return res.status(500).send(e.message)
   }
-};
+}
+
+export const postSignin = async (req, res) => {
+  const { user } = res.locals
+  const secretKey = process.env.JWT_SECRET_KEY
+  const token = jwt.sign(user, secretKey)
+  try {
+    await db.query(
+      `
+      INSERT INTO sessions (user_id, token)
+      VALUES ($1, $2)`,
+      [user.id, token],
+    )
+    delete user.id
+    res.send({ ...user, token })
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,16 +1,25 @@
-import { Router } from "express";
+import { Router } from "express"
 
-import { postUser } from "../controllers/authController.js";
-import { signupMiddleware } from "../middlewares/authMiddleware.js";
-import { validateSchema } from "../middlewares/schemaValidator.js";
-import { signupSchema } from "../schemas/authSchema.js";
+import { postSignin, postUser } from "../controllers/authController.js"
+import {
+  signinMiddleware,
+  signupMiddleware,
+} from "../middlewares/authMiddleware.js"
+import { validateSchema } from "../middlewares/schemaValidator.js"
+import { signinSchema, signupSchema } from "../schemas/authSchema.js"
 
-const authRouter = Router();
+const authRouter = Router()
 authRouter.post(
   "/signup",
   validateSchema(signupSchema),
   signupMiddleware,
-  postUser
-);
+  postUser,
+)
+authRouter.post(
+  "/signin",
+  validateSchema(signinSchema),
+  signinMiddleware,
+  postSignin,
+)
 
-export default authRouter;
+export default authRouter
