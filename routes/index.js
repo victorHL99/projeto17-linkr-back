1935cd8d3e5b6a0b387f4f0b3242a75648256aac
lemoscaio <caio_lemos94@hotmail.com>
2022-06-17 01:15:20 -0300

    add url-metada response on getPosts requests

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-import { Router } from "express";
-import authRouter from "./authRouter.js";
-import postsRouter from "./feedRouter.js";
-import publishRouter from "./publishRouter.js";
-import trendingRouter from "./trendingRouter.js";
+import { Router } from "express"
+import authRouter from "./authRouter.js"
+import postsRouter from "./postsRouter.js"
+import publishRouter from "./publishRouter.js"
+import trendingRouter from "./trendingRouter.js"
 
-const router = Router();
+const router = Router()
 
-router.use(publishRouter);
-router.use(authRouter);
-router.use(trendingRouter);
-router.use(postsRouter);
+router.use(publishRouter)
+router.use(authRouter)
+router.use(trendingRouter)
+router.use(postsRouter)
 
-export default router;
+export default router
