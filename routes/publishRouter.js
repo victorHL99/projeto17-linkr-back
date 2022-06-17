7c28eb5c72da06ee63366f2807b3d2eddc1b9919
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-17 16:31:44 -0300

    <fix> : bug fixed - token

--- routes/publishRouter.js
+++ routes/publishRouter.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import { validateSchema } from "../middlewares/schemaValidator.js";
+import { tokenValidation } from "../middlewares/tokenValidation.js";
 
 import publishSchema from "../schemas/publishSchema.js";
 
 import { createPublish } from "../controllers/publishController.js";
 
 const publishRouter = Router();
-//console.log(" Passando pela rota de publicação ");
 
-publishRouter.post("/publish", validateSchema(publishSchema), createPublish);
+publishRouter.post("/publish", validateSchema(publishSchema), tokenValidation, createPublish);
 
 export default publishRouter;
