9091ded77d2950cd3e930dc756f0c1b45f2b2fa9
SADoug <douglastec2@gmail.com>
2022-06-17 11:49:58 -0300

    first

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -5,12 +5,7 @@ export async function addLike(req, res) {
     const { user_id, post_id } = req.body
 
     try {
-        
-        const likes = await db.query(`
-        SELECT COUNT("post_id") AS likes
-        FROM likes
-        WHERE post_id = $1`, [post_id]);
-        
+
         await db.query(`
         INSERT INTO likes ("user_id","post_id")
         VALUES($1,$2);
@@ -18,11 +13,18 @@ export async function addLike(req, res) {
             user_id,
             post_id
         ]);
-        console.log("DEU BOM");
-        res.status(201).send(likes.rows[0]);
+        const likes = await db.query(`
+        SELECT users.username, posts.id as post_id
+        FROM likes
+        JOIN users ON users.id = likes.user_id
+        JOIN posts ON posts.id = likes.post_id
+        WHERE likes.post_id=$1
+        GROUP BY post_id, posts.id,users.id,users."username"; `, [post_id]);
+
+        res.status(201).send(likes.rows);
 
     } catch (error) {
-        console.log("DEU RUIM");
+
         console.log(error)
         res.status(422).send(error)
     }
@@ -32,8 +34,18 @@ export async function deleteLike(req, res) {
 
     const { user_id, post_id } = req.body
     try {
-        const likes = await db.query(`
+        await db.query(`
         DELETE FROM likes WHERE user_id = $1 AND post_id = $2;`, [user_id, post_id]);
+
+        const likes = await db.query(`
+        SELECT users.username, posts.id as post_id
+        FROM likes
+        JOIN users ON users.id = likes.user_id
+        JOIN posts ON posts.id = likes.post_id
+        WHERE likes.post_id=$1
+        GROUP BY post_id, posts.id,users.id,users."username"; `, [post_id]);
+
+        res.status(201).send(likes.rows);
 
     } catch (error) {
         res.status(422).send(error)
