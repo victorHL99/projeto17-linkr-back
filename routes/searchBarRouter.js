8a32d3ee8b816459938d45d2c90e76feb228bb72
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-19 18:33:27 -0300

    <fix> : modified searchbarRouter

--- routes/searchBarRouter.js
+++ routes/searchBarRouter.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 
-import { getUserBySearch } from "../controllers/searchBarController.js";
+import { getUserBySearch } from "../controllers/userController.js";
 
 const searchBarRouter = Router();
 
-searchBarRouter.get("/searchBar/:name", tokenValidation, getUserBySearch);
+searchBarRouter.get("/search/:name", tokenValidation, getUserBySearch);
 
 export default searchBarRouter;
