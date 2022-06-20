b5a930346102ccf39d5d6df8edfd83b226c418bc
lemoscaio <caio_lemos94@hotmail.com>
2022-06-20 06:15:42 -0300

    fix trendingController making a wrong token validation

--- controllers/trendingController.js
+++ controllers/trendingController.js
@@ -1,25 +1,13 @@
-import { getTrendingHashtags } from "../repositories/trendingRepository.js";
+import { getTrendingHashtags } from "../repositories/trendingRepository.js"
 
-import sessionsRepository from "../repositories/sessionsRepository.js";
+export async function getTrendings(req, res) {
+  const LIMIT_TRENDINGS = 10
 
-export async function getTrendinds(req, res) {
-    const {authorization} = req.headers;
-    const token = authorization?.replace('Bearer ', '').trim();
+  try {
+    const { rows: trendings } = await getTrendingHashtags(LIMIT_TRENDINGS)
 
-    const LIMIT_TRENDINGS = 10;
-
-    try {
-        const resultVerifyToken = await sessionsRepository.verifyToken(token);
-
-        if(resultVerifyToken.rows.length === 0){
-            return res.status(401).send("Token inválido");
-        }
-
-        const { rows: trendings} = await getTrendingHashtags(LIMIT_TRENDINGS);
-
-        res.send(trendings);
-
-    } catch(err) {
-        return res.status(500).send(err.message);
-    }
+    res.send(trendings)
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
 }
