36d72218d10f18db8b6ea1bf7cdc78a2e52712fc
lemoscaio <caio_lemos94@hotmail.com>
2022-06-20 07:20:52 -0300

    fix: fix data sent on response and refactors trendingRepository structure

--- controllers/trendingController.js
+++ controllers/trendingController.js
@@ -1,10 +1,12 @@
-import { getTrendingHashtags } from "../repositories/trendingRepository.js"
+import { trendingRepository } from "../repositories/trendingRepository.js"
 
 export async function getTrendings(req, res) {
   const LIMIT_TRENDINGS = 10
 
   try {
-    const { rows: trendings } = await getTrendingHashtags(LIMIT_TRENDINGS)
+    const { rows: trendings } = await trendingRepository.getTrendingHashtags(
+      LIMIT_TRENDINGS,
+    )
 
     res.send(trendings)
   } catch (err) {
