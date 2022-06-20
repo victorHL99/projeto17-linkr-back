4a68ab312011296fd9619fdca739362849ed57de
SADoug <douglastec2@gmail.com>
2022-06-20 16:57:33 -0300

    commit with all the project functions

--- schemas/newPostSchema.js
+++ schemas/newPostSchema.js
@@ -1,10 +1,7 @@
 import Joi from "joi"
 
 const newPostSchema = Joi.object({
-  sharedUrl: Joi.string()
-    .required()
-    .pattern(/^(http|https):\/\/[^ "]+$/),
-  message: Joi.string(),
+  message: Joi.any(),
 })
 
 export default newPostSchema
