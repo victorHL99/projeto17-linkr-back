9329d342869c772ce39f7153b6132d121a108e7a
SADoug <douglastec2@gmail.com>
2022-06-22 01:05:21 -0300

    fixing the post

--- schemas/newPostSchema.js
+++ schemas/newPostSchema.js
@@ -1,8 +1,10 @@
 import Joi from "joi"
 
 const newPostSchema = Joi.object({
-  sharedUrl: Joi.any(),
-  message: Joi.any()
+  sharedUrl: Joi.string()
+    .required()
+    .pattern(/^(http|https):\/\/[^ "]+$/),
+  message: Joi.any(),
 })
 
 export default newPostSchema
