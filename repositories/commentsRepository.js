4ffc7f2b630d052dce4659481eae26d8fba999f2
Mariana Ito <marianaitopinto@gmail.com>
2022-06-22 13:36:48 -0300

    feat: list comments

---
+++ repositories/commentsRepository.js
@@ -0,0 +1,18 @@
+import db from "../config/db.js"
+
+export async function getComments(id, userId) {
+    return db.query(`
+    SELECT 
+    comments.id
+    , comments.post_id as "postId"
+    , comments.user_id as "userId"
+    , comments.message
+    , users.profile_image as "userImage"
+    , users.username
+    FROM comments
+    JOIN users on user.id=comments.user_id
+    WHERE
+    post_id = $1
+    `, [id]);
+}
+
