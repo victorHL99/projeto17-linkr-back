b255f2b76c4f90265997a020549c759c608ab0de
Mariana Ito <marianaitopinto@gmail.com>
2022-06-24 03:45:26 -0300

    fixing query to create comment

--- repositories/commentsRepository.js
+++ repositories/commentsRepository.js
@@ -28,7 +28,7 @@ async function countComments(id) {
 }
 
 
-async function insertComment(id, message, userId) {
+async function insertComment(id, userId, message) {
   return db.query(`INSERT INTO comments
     (post_id, user_id, message)
     VALUES ($1, $2, $3)`, [id, userId, message])
